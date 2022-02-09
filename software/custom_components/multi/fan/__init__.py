06ca04c095809426828f899623d8e47227ae8c67
Andreas Frisch <fraxinas@purplegecko.de>
2022-02-09 21:44:00 +0100

    custom_components: Move the update_interval into main

--- software/custom_components/multi/fan/__init__.py
+++ software/custom_components/multi/fan/__init__.py
@@ -4,6 +4,8 @@ import esphome.config_validation as cv
 from esphome.components import fan, output, sensor
 from esphome.components.pid import climate
 from esphome.const import (
+    CONF_DISABLED_BY_DEFAULT,
+    CONF_NAME,
     CONF_OUTPUT_ID,
     CONF_OUTPUT,
     CONF_WEIGHT,
@@ -19,6 +21,7 @@ _LOGGER = logging.getLogger(__name__)
 
 MultiFan = multi_ns.class_("MultiFan", sensor.Sensor, cg.PollingComponent)
 
+CONF_FAN_ID = 'fan_id'
 CONF_HYGRO = 'hygrostat'
 CONF_THERMO = 'thermostat'
 CONF_VICINITY = 'vicinity'
@@ -28,26 +31,36 @@ PidConf = multi_ns.struct("PidConf")
 ITEM_SCHEMA = cv.Schema({
     cv.Required(CONF_ID): cv.use_id(climate.PIDClimate),
     cv.Optional(CONF_WEIGHT, default=1.0): cv.positive_float,
-}).extend(cv.polling_component_schema("10s"))
+})
 
-
-CONFIG_SCHEMA = fan.FAN_SCHEMA.extend(
+CONFIG_SCHEMA = cv.ENTITY_BASE_SCHEMA.extend(
     {
-        cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(MultiFan),
+        cv.GenerateID(CONF_FAN_ID): cv.declare_id(fan.FanState),
+        cv.GenerateID(CONF_ID): cv.declare_id(MultiFan),
         cv.Required(CONF_OUTPUT): cv.use_id(output.FloatOutput),
         cv.Required(CONF_HYGRO): cv.ensure_list(ITEM_SCHEMA),
         cv.Required(CONF_THERMO): cv.ensure_list(ITEM_SCHEMA),
         cv.Optional(CONF_VICINITY): cv.use_id(sensor.Sensor),
-        cv.Optional(CONF_WIND_SPEED): cv.use_id(sensor.Sensor),    }
-).extend(cv.COMPONENT_SCHEMA)
+        cv.Optional(CONF_WIND_SPEED): cv.use_id(sensor.Sensor)
+    }
+).extend(cv.COMPONENT_SCHEMA).extend(cv.polling_component_schema("10s"))
 
 ITEMLIST = (CONF_HYGRO, CONF_THERMO)
 
 async def to_code(config):
     print("config:", config)
+
+    fan_config = {
+        CONF_ID: config[CONF_FAN_ID],
+        CONF_NAME: config[CONF_NAME]+'_fanstate',
+        CONF_DISABLED_BY_DEFAULT: config[CONF_DISABLED_BY_DEFAULT]
+    }
+    print("fan_config", fan_config)
+
+    fan_state = cg.new_Pvariable(fan_config[CONF_ID])
     output_ = await cg.get_variable(config[CONF_OUTPUT])
-    state = await fan.create_fan_state(config)
-    var = cg.new_Pvariable(config[CONF_OUTPUT_ID], state, output_)
+    await fan.register_fan(fan_state, fan_config)
+    var = cg.new_Pvariable(config[CONF_ID], fan_state, output_)
     await cg.register_component(var, config)
 
     for (conf, item) in zip([config.get(item, []) for item in ITEMLIST], ITEMLIST):
